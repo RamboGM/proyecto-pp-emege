f5f1421e10924c16a3434150806fa73025530204
RamboGM <gaspar.rambo@gmail.com>
2023-09-24 20:30:02 -0300

    Cambios de ultima semana

--- src/componentes/Catalogo.jsx
+++ src/componentes/Catalogo.jsx
@@ -5,11 +5,10 @@ import Footer from './Footer';
 import '../Stylesheets/Catalogo.css';
 import '../Stylesheets/Popup.css';
 
-
 function CatalogoProductos() {
   const [productos, setProductos] = useState(Productos);
   const [carrito, setCarrito] = useState([]);
-  const [showPopup, setShowPopup] = useState(false); // Estado para controlar la visibilidad del popup
+  const [showPopup, setShowPopup] = useState(false);
   const numeroWhatsApp = '+543544523837';
 
   useEffect(() => {
@@ -23,51 +22,153 @@ function CatalogoProductos() {
     localStorage.setItem('carrito', JSON.stringify(carrito));
   }, [carrito]);
 
-  const addToCart = (producto) => {
+  const addToCart = (producto, cantidad) => {
     const isInCart = carrito.some((item) => item.id === producto.id);
 
     if (!isInCart) {
-      setCarrito([...carrito, producto]);
-      setShowPopup(true); // Mostrar el popup cuando se agrega al carrito
-      // Establecer un temporizador para ocultar el popup después de unos segundos
+      setCarrito([...carrito, { ...producto, cantidad }]);
+      setShowPopup(true);
       setTimeout(() => {
         setShowPopup(false);
-      }, 2000); // Cambia el tiempo (en milisegundos) según tus preferencias
+      }, 2000);
+    } else {
+      const updatedCart = carrito.map((item) => {
+        if (item.id === producto.id) {
+          return { ...item, cantidad: item.cantidad + cantidad };
+        }
+        return item;
+      });
+      setCarrito(updatedCart);
     }
+  };
+
+  const removeFromCart = (producto) => {
+    const updatedCart = carrito.filter((item) => item.id !== producto.id);
+    setCarrito(updatedCart);
+  };
+
+  const clearCart = () => {
+    setCarrito([]);
   };
 
   const compartirCarritoWhatsApp = () => {
     let mensaje = `¡Hola! Estoy interesado en los siguientes productos:\n\n`;
     carrito.forEach((producto) => {
-      mensaje += `${producto.name} - Precio: $${producto.price}\n`;
+      mensaje += `${producto.name} - Cantidad: ${producto.cantidad} - Precio: $${producto.price}\n`;
     });
 
-    const totalCompra = carrito.reduce((total, producto) => total + producto.price, 0);
+    const totalCompra = carrito.reduce(
+      (total, producto) => total + producto.price * producto.cantidad,
+      0
+    );
     mensaje += `\nTotal de la compra: $${totalCompra.toFixed(2)}`;
 
-    const url = `https://api.whatsapp.com/send?phone=${encodeURIComponent(numeroWhatsApp)}&text=${encodeURIComponent(mensaje)}`;
+    const url = `https://api.whatsapp.com/send?phone=${encodeURIComponent(
+      numeroWhatsApp
+    )}&text=${encodeURIComponent(mensaje)}`;
     window.open(url, '_blank');
   };
+
+  const totalCarrito = carrito.reduce(
+    (total, producto) => total + producto.price * producto.cantidad,
+    0
+  );
 
   return (
     <div>
       <div className='container-titulo'>
         <h1>CATÁLOGO</h1>
       </div>
-      <div className="container">
+      <div className="container-cards">
         <div className="row">
           {productos.map((producto) => (
             <div className="col-md-4 mb-4" key={producto.id}>
               <div className="card">
                 <Link to={`/product/${producto.id}`} className="card-link">
-                  <img src={producto.image} className="card-img-top" alt={producto.name} />
+                  {/* Carrusel de Bootstrap */}
+                  <div
+                    id={`carousel-${producto.id}`}
+                    className="carousel slide"
+                    data-bs-ride="carousel"
+                  >
+                    <div className="carousel-inner">
+                      {producto.images.map((image, index) => (
+                        <div
+                          key={index}
+                          className={`carousel-item ${
+                            index === 0 ? 'active' : ''
+                          }`}
+                        >
+                          <img
+                            src={image}
+                            alt={producto.name}
+                            className="d-block w-100"
+                          />
+                        </div>
+                      ))}
+                    </div>
+                    <button
+                      className="carousel-control-prev"
+                      type="button"
+                      data-bs-target={`#carousel-${producto.id}`}
+                      data-bs-slide="prev"
+                    >
+                      <span
+                        className="carousel-control-prev-icon"
+                        aria-hidden="true"
+                      ></span>
+                      <span className="visually-hidden">Previous</span>
+                    </button>
+                    <button
+                      className="carousel-control-next"
+                      type="button"
+                      data-bs-target={`#carousel-${producto.id}`}
+                      data-bs-slide="next"
+                    >
+                      <span
+                        className="carousel-control-next-icon"
+                        aria-hidden="true"
+                      ></span>
+                      <span className="visually-hidden">Next</span>
+                    </button>
+                  </div>
                 </Link>
                 <div className="card-body">
                   <h5 className="card-title">{producto.name}</h5>
+                  <p className="card-text">{producto.description}</p>
                   <p className="card-text">Precio: ${producto.price}</p>
+                  {/* Selector de cantidad */}
+                  <div className="mb-3">
+                    <label
+                      htmlFor={`cantidad-${producto.id}`}
+                      className="form-label"
+                    >
+                      Cantidad:
+                    </label>
+                    <select
+                      id={`cantidad-${producto.id}`}
+                      className="form-select"
+                      value={producto.cantidad}
+                      onChange={(e) => {
+                        const newCantidad = parseInt(e.target.value);
+                        const updatedProductos = productos.map((prevProducto) =>
+                          prevProducto.id === producto.id
+                            ? { ...prevProducto, cantidad: newCantidad }
+                            : prevProducto
+                        );
+                        setProductos(updatedProductos);
+                      }}
+                    >
+                      {Array.from({ length: 10 }, (_, i) => (
+                        <option key={i} value={i + 1}>
+                          {i + 1}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
                   <button
                     className="btn btn-cart"
-                    onClick={() => addToCart(producto)}
+                    onClick={() => addToCart(producto, producto.cantidad)}
                   >
                     Agregar al Carrito
                   </button>
@@ -77,17 +178,52 @@ function CatalogoProductos() {
           ))}
         </div>
         <h1 className='carrito-compras-titulo'>Carrito de Compras</h1>
-        <ul>
-          {carrito.map((producto) => (
-            <li key={producto.id}>
-              {producto.name} - Precio: ${producto.price}
-            </li>
-          ))}
-        </ul>
-        {carrito.length > 0 && (
-          <button className="btn btn-whatsapp" onClick={compartirCarritoWhatsApp}>
-            Compartir Carrito por WhatsApp
-          </button>
+        {carrito.length > 0 ? (
+          <div>
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Producto</th>
+                  <th>Cantidad</th>
+                  <th>Precio</th>
+                  <th>Total</th>
+                  <th>Eliminar</th>
+                </tr>
+              </thead>
+              <tbody>
+                {carrito.map((producto) => (
+                  <tr key={producto.id}>
+                    <td>{producto.name}</td>
+                    <td>{producto.cantidad}</td>
+                    <td>${producto.price}</td>
+                    <td>${(producto.price * producto.cantidad).toFixed(2)}</td>
+                    <td>
+                      <button
+                        className="btn btn-danger"
+                        onClick={() => removeFromCart(producto)}
+                      >
+                        X
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <div className='text-center'>
+              <p>Total del Carrito: ${totalCarrito.toFixed(2)}</p>
+            </div>
+            <button className="btn btn-danger" onClick={clearCart}>
+              Vaciar Carrito
+            </button>
+            <button
+              className="btn btn-whatsapp"
+              onClick={compartirCarritoWhatsApp}
+            >
+              Compartir Carrito por WhatsApp
+            </button>
+          </div>
+        ) : (
+          <p>Tu carrito está vacío.</p>
         )}
       </div>
       <div>
@@ -95,7 +231,8 @@ function CatalogoProductos() {
       </div>
       {showPopup && (
         <div className="popup">
-          <p>Agregado al carrito</p>
+          <p>Agregado al carrito 😊</p>
+          <h2>⬇️</h2>
         </div>
       )}
     </div>
@@ -103,7 +240,5 @@ function CatalogoProductos() {
 }
 
 export default CatalogoProductos;
-
-
 
 
